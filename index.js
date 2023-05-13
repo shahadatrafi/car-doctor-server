56ca6d4565d5e88b97c89825e2a0e7780f21f0b3
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-14 00:39:08 +0600

    service loaded done for checkout

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,20 @@ async function run() {
       const cursor = servicesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        projection: { title: 1, img: 1, price: 1, service_id: 1 },
+      };
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result);
+    });
+
+    app.post('/checkout', async (req, res) => {
+      
     })
 
     // Send a ping to confirm a successful connection
