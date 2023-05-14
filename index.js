a73104886bd3f0ab91c36b9a62ec8a6cd5159d42
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-14 13:31:54 +0600

    services order added successfully in mongodb

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 console.log(process.env.DB_USER);
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jmzo55h.mongodb.net/?retryWrites=true&w=majorit`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jmzo55h.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +30,7 @@ async function run() {
       await client.connect();
       
     const servicesCollection = client.db("carDoctor").collection("services");
+    const orderCollection = client.db("carDoctor").collection("orders");
 
     app.get('/services', async (req, res) => {
       const cursor = servicesCollection.find();
@@ -41,15 +42,18 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const options = {
-        projection: { title: 1, img: 1, price: 1, service_id: 1 },
+        projection: { title: 1, img: 1, price: 1, service_id: 1, img: 1 },
       };
       const result = await servicesCollection.findOne(query, options);
       res.send(result);
     });
 
     app.post('/checkout', async (req, res) => {
-      
-    })
+      const orderItem = req.body;
+      console.log(orderItem);
+      const result = await orderCollection.insertOne(orderItem)
+      res.send(result)
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
