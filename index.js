b54539b650b4f8f22ad56609ded20238a5c31804
ShahadatRafi <mdshahadathossenrafi@gmail.com>
2023-05-16 21:06:44 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -22,6 +23,24 @@ const client = new MongoClient(uri, {
   }
 });
 
+const verifyJWT = (req, res, next) => {
+  // console.log(req.headers.authorization)
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  };
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      console.log(error);
+      return res.status(403).send({ error: true, message: 'unauthorized access' })
+      
+    };
+    req.decoded = decoded;
+    next();
+  })
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -29,6 +48,18 @@ async function run() {
       
     const servicesCollection = client.db("carDoctor").collection("services");
     const orderCollection = client.db("carDoctor").collection("orders");
+
+    // jwt routes
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      console.log(user)
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+      res.send({token});
+
+    })
+
+    // services routes
 
     app.get('/services', async (req, res) => {
       const cursor = servicesCollection.find();
@@ -46,8 +77,14 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/checkout', async (req, res) => {
-      console.log(req.query.email);
+
+    // Checkout routes
+
+    app.get('/checkout', verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      if (decoded.email !== req.query.email) {
+        return res.send({ error: 1, message: 'forbidden error' });
+      }
       let query = {};
       if (req.query?.email) {
         query = { CustomerEmail: req.query.email };
@@ -84,6 +121,8 @@ async function run() {
       const result = await orderCollection.insertOne(orderItem)
       res.send(result)
     });
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
